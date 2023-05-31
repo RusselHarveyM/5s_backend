76d2990b9128f951c741a18da8783f6f615c97d0
Russel Harvey Mercado IV <russelharvey.mercado@cit.edu>
2023-05-31 08:07:16 +0800

    make names unique

--- 5sDB/Table/Room.sql
+++ 5sDB/Table/Room.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Rooms](
     [Id] INT NOT NULL IDENTITY(1,1), 
     [BuildingId]    INT  ,
-    [RoomNumber]    VARCHAR(255) ,
+    [RoomNumber]    VARCHAR(255) UNIQUE,
     [Image] VARBINARY(MAX),
     PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_RoomBuilding] FOREIGN KEY ([BuildingId]) REFERENCES [dbo].[Building] ([Id]) ON DELETE CASCADE,
