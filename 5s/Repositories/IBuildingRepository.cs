0b5d19882e16360251509bce35e4c57a0bb76d59
Raymart-Leyson <raymart.leyson@cit.edu>
2023-10-07 14:36:16 +0800

    Update Building comment

--- 5s/Repositories/IBuildingRepository.cs
+++ 5s/Repositories/IBuildingRepository.cs
@@ -7,7 +7,7 @@ namespace _5s.Repositories
         /// <summary>
         /// Create a building
         /// </summary>
-        /// <param name="building"></param>
+        /// <param name="building">Building details</param>
         /// <returns>Returns a building model</returns>
         public Task<int> CreateBuilding(Building building);
         /// <summary>
@@ -18,26 +18,26 @@ namespace _5s.Repositories
         /// <summary>
         /// Get building by Id
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">Building Id</param>
         /// <returns>Returns building details</returns>
         public Task<Building> GetBuildingById(int id);
         /// <summary>
         /// Get building by Name
         /// </summary>
-        /// <param name="name"></param>
+        /// <param name="name">Building name</param>
         /// <returns>Returns building details</returns>
         public Task<Building> GetBuildingByName(string name);
         /// <summary>
         /// Updates an existing Building
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="updatedBuilding"></param>
+        /// <param name="id">Building Id</param>
+        /// <param name="updatedBuilding">new Building Details</param>
         /// <returns>Returns the Id of the updated building</returns>
         public Task<int> UpdateBuilding(int id, Building updatedBuilding);
         /// <summary>
         /// Delete a building
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">Building Id</param>
         public Task DeleteBuilding(int id);
     }
 }
