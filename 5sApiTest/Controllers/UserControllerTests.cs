9ee983f83adcbee1cca24c2ab1403837f28c38dd
Hareram Chua <hareram.chua@cit.edu>
2023-11-29 22:53:45 +0800

    add missing unit test for UserControllerTests

--- 5sApiTest/Controllers/UserControllerTests.cs
+++ 5sApiTest/Controllers/UserControllerTests.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using _5s.Controllers;
 using _5s.Model;
 using _5s.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
@@ -54,10 +55,36 @@ namespace _5sApiTest.Controllers
         }
 
         [Fact]
-        public async Task GetUser_ExistingUsers_ReturnsOkResultWithUsers()
+        public async Task CreateUser_ServiceError_ReturnsInternalServerError()
         {
             // Arrange
-            var expectedUsers = new List<User> { /* Define user list */ };
+            var newUser = new User {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Role = "admin"
+            };
+            _userServiceMock.Setup(service => service.CreateUser(newUser))
+                            .ThrowsAsync(new Exception("Simulated service error"));
+
+            var controller = new UserController(_userServiceMock.Object);
+
+            // Act
+            var result = await controller.CreateUser(newUser) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAllUser_ExistingUsers_ReturnsOkResultWithUsers()
+        {
+            // Arrange
+            var expectedUsers = new List<User> {
+                new User { Id = 1, FirstName = "John", LastName = "Doe", Role = "user" },
+                new User { Id = 2, FirstName = "Juan", LastName = "Dela Cruz", Role = "admin" }
+            };
             _userServiceMock.Setup(service => service.GetAllUser())
                 .ReturnsAsync(expectedUsers);
 
@@ -69,15 +96,37 @@ namespace _5sApiTest.Controllers
             Assert.Equal(200, result.StatusCode);
             Assert.NotNull(result.Value);
             Assert.IsType<List<User>>(result.Value);
-            // Further assertions based on the expected user list content or structure
+        }
+
+        [Fact]
+        public async Task GetAllUser_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            _userServiceMock.Setup(service => service.GetAllUser())
+                            .ThrowsAsync(new Exception("Simulated service error"));
+
+            var controller = new UserController(_userServiceMock.Object);
+
+            // Act
+            var result = await controller.GetUser() as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
         }
 
         [Fact]
         public async Task GetUserById_ExistingUserId_ReturnsOkResultWithUser()
         {
             // Arrange
-            int userId = 1; // Define an existing user ID
-            var expectedUser = new User { /* Define user properties */ };
+            int userId = 1;
+            var expectedUser = new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Role = "user"
+            };
             _userServiceMock.Setup(service => service.GetUserById(userId))
                 .ReturnsAsync(expectedUser);
 
@@ -89,7 +138,25 @@ namespace _5sApiTest.Controllers
             Assert.Equal(200, result.StatusCode);
             Assert.NotNull(result.Value);
             Assert.IsType<User>(result.Value);
-            // Further assertions based on the expected user content or structure
+        }
+
+        [Fact]
+        public async Task GetUserById_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            int userId = 1;
+
+            _userServiceMock.Setup(service => service.GetUserById(userId))
+                            .ThrowsAsync(new Exception("Simulated service error"));
+
+            var controller = new UserController(_userServiceMock.Object);
+
+            // Act
+            var result = await controller.GetUser(userId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
         }
 
         [Fact]
@@ -129,11 +196,70 @@ namespace _5sApiTest.Controllers
         }
 
         [Fact]
+        public async Task UpdateUser_UserNotFound_ReturnsNotFound()
+        {
+            // Arrange
+            int userId = 1;
+            var updatedUser = new User
+            {
+                Id = userId,
+                FirstName = "Updated",
+                LastName = "User",
+                Role = "admin"
+            };
+
+            _userServiceMock.Setup(service => service.GetUserById(userId)).ReturnsAsync((User)null);
+
+            var controller = new UserController(_userServiceMock.Object);
+
+            // Act
+            var result = await controller.UpdateUser(userId, updatedUser) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task UpdateUser_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            int userId = 1;
+            var updatedUser = new User
+            {
+                Id = userId,
+                FirstName = "Updated",
+                LastName = "User",
+                Role = "admin"
+            };
+
+            _userServiceMock.Setup(service => service.GetUserById(userId))
+                           .ReturnsAsync(new User());
+
+            _userServiceMock.Setup(service => service.UpdateUser(userId, updatedUser)).ThrowsAsync(new Exception("Simulated service error"));
+
+            var controller = new UserController(_userServiceMock.Object);
+
+            // Act
+            var result = await controller.UpdateUser(userId, updatedUser) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
+        }
+
+        [Fact]
         public async Task DeleteUser_ExistingUserId_ReturnsOkResultWithSuccessMessage()
         {
             // Arrange
             int userId = 1; // Define an existing user ID
-            var existingUser = new User { /* Define existing user properties */ };
+            var existingUser = new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Role = "user"
+            };
             _userServiceMock.Setup(service => service.GetUserById(userId))
                 .ReturnsAsync(existingUser);
 
@@ -144,6 +270,46 @@ namespace _5sApiTest.Controllers
             Assert.NotNull(result);
             Assert.Equal(200, result.StatusCode);
             Assert.Equal("User successfully deleted", result.Value);
+        }
+
+        [Fact]
+        public async Task DeleteUser_UserNotFound_ReturnsNotFound()
+        {
+            // Arrange
+            int userId = 1;
+
+            _userServiceMock.Setup(service => service.GetUserById(userId)).ReturnsAsync((User)null);
+
+            var controller = new UserController(_userServiceMock.Object);
+
+            // Act
+            var result = await controller.DeleteUser(userId) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteUser_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            int userId = 1;
+
+            _userServiceMock.Setup(service => service.GetUserById(userId))
+                            .ReturnsAsync(new User { Id = userId });
+
+            _userServiceMock.Setup(service => service.DeleteUser(userId))
+                            .ThrowsAsync(new Exception("Simulated service error"));
+
+            var controller = new UserController(_userServiceMock.Object);
+
+            // Act
+            var result = await controller.DeleteUser(userId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
         }
     }
 }
