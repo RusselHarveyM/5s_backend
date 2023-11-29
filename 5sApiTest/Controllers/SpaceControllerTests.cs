ad79122595696cc319bb0c0bb19d07864d91433b
Hareram Chua <hareram.chua@cit.edu>
2023-11-29 22:28:30 +0800

    modify unit test for SpaceControllerTest and in SpaceController

--- 5sApiTest/Controllers/SpaceControllerTests.cs
+++ 5sApiTest/Controllers/SpaceControllerTests.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using _5s.Controllers;
 using _5s.Model;
 using _5s.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
@@ -39,7 +40,54 @@ namespace _5sApiTest.Controllers
         }
 
         [Fact]
-        public async Task GetSpace_ReturnsOkObjectResult()
+        public async Task CreateSpace_InvalidData_ReturnsBadRequest()
+        {
+            // Arrange
+            var space = new Space
+            {
+                Id = 0,
+                Name = "Table",
+                Pictures = new List<byte[]>(),
+                RoomId = 1
+            };
+
+            var controller = new SpaceController(_spaceServiceMock.Object);
+            controller.ModelState.AddModelError("Id", "Id should not be <= 0");
+
+            // Act
+            var result = await controller.CreateSpace(space) as BadRequestObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateSpace_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            var space = new Space
+            {
+                Id = 1,
+                Name = "SampleSpace",
+                Pictures = new List<byte[]>(),
+                RoomId = 2
+            };
+
+            _spaceServiceMock.Setup(service => service.CreateSpace(It.IsAny<Space>())).ThrowsAsync(new Exception("Simulated service error"));
+
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.CreateSpace(space) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAllSpace_ReturnsOkObjectResult()
         {
             // Arrange
             var spaces = new List<Space> { new Space { Id = 1, Name = "Meeting Room", Pictures = new List<byte[]>(), RoomId = 1 } };
@@ -52,6 +100,37 @@ namespace _5sApiTest.Controllers
             Assert.NotNull(result);
             Assert.Equal(200, result.StatusCode);
             Assert.NotNull(result.Value);
+        }
+
+        [Fact]
+        public async Task GetAllSpace_NoSpacesFound_ReturnsNotFound()
+        {
+            // Arrange
+            List<Space> emptySpaceList = new List<Space>();
+            _spaceServiceMock.Setup(service => service.GetAllSpace()).ReturnsAsync(emptySpaceList);
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.GetSpace() as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAllSpace_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            _spaceServiceMock.Setup(service => service.GetAllSpace()).ThrowsAsync(new Exception("Simulated service error"));
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.GetSpace() as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
         }
 
         [Fact]
@@ -71,22 +150,111 @@ namespace _5sApiTest.Controllers
             Assert.NotNull(result.Value);
         }
 
-        /*[Fact]
-        public async Task UpdateSpace_ExistingId_ReturnsOkObjectResult()
+        [Fact]
+        public async Task GetSpaceById_NoSpaceFound_ReturnsNotFound()
         {
             // Arrange
-            int spaceId = 1;
-            var updatedSpace = new Space { Id = spaceId, Name = "New Meeting Room", Pictures = new List<byte[]>(), RoomId = 1 };
-            _spaceServiceMock.Setup(service => service.UpdateSpace(spaceId, It.IsAny<Space>())).ReturnsAsync(1);
+            int sampleId = -1;
+            _spaceServiceMock.Setup(service => service.GetSpaceById(sampleId)).ReturnsAsync((Space)null);
+            var controller = new SpaceController(_spaceServiceMock.Object);
 
             // Act
-            var result = await _spaceController.UpdateSpace(spaceId, updatedSpace) as OkObjectResult;
+            var result = await controller.GetSpace(sampleId) as NotFoundResult;
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(200, result.StatusCode);
-            Assert.NotNull(result.Value);
-        }*/
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetSpaceById_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            int sampleId = 1; // Assuming this ID exists or can be found
+            _spaceServiceMock.Setup(service => service.GetSpaceById(sampleId)).ThrowsAsync(new Exception("Simulated service error"));
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.GetSpace(sampleId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task UpdateSpace_ValidSpace_ReturnsOk()
+        {
+            // Arrange
+            int sampleId = 1;
+            var spaceToUpdate = new Space
+            {
+                Id = sampleId,
+                Name = "UpdatedSpace",
+                Pictures = new List<byte[]>(),
+                RoomId = 2
+            };
+
+            _spaceServiceMock.Setup(service => service.GetSpaceById(sampleId)).ReturnsAsync(spaceToUpdate);
+            _spaceServiceMock.Setup(service => service.UpdateSpace(sampleId, spaceToUpdate)).ReturnsAsync(1);
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.UpdateSpace(sampleId, spaceToUpdate) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task UpdateSpace_SpaceNotFound_ReturnsNotFound()
+        {
+            int nonExistentId = 999;
+            var spaceToUpdate = new Space
+            {
+                Id = nonExistentId,
+                Name = "UpdatedSpace",
+                Pictures = new List<byte[]>(),
+                RoomId = 2
+            };
+
+            _spaceServiceMock.Setup(service => service.GetSpaceById(nonExistentId)).ReturnsAsync((Space)null);
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.UpdateSpace(nonExistentId, spaceToUpdate) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task UpdateSpace_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            int sampleId = 1;
+            var spaceToUpdate = new Space
+            {
+                Id = sampleId,
+                Name = "UpdatedSpace",
+                Pictures = new List<byte[]>(),
+                RoomId = 2
+            };
+
+            _spaceServiceMock.Setup(service => service.GetSpaceById(sampleId)).ReturnsAsync(spaceToUpdate);
+            _spaceServiceMock.Setup(service => service.UpdateSpace(sampleId, spaceToUpdate))
+                             .ThrowsAsync(new Exception("Simulated service error"));
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.UpdateSpace(sampleId, spaceToUpdate) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
+        }
 
         [Fact]
         public async Task DeleteSpace_ExistingId_ReturnsOkObjectResult()
@@ -102,6 +270,50 @@ namespace _5sApiTest.Controllers
             Assert.NotNull(result);
             Assert.Equal(200, result.StatusCode);
             Assert.NotNull(result.Value);
+        }
+
+        [Fact]
+        public async Task DeleteSpace_SpaceNotFound_ReturnsNotFound()
+        {
+            // Arrange
+            int nonExistentId = -1;
+
+            _spaceServiceMock.Setup(service => service.GetSpaceById(nonExistentId)).ReturnsAsync((Space)null);
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.DeleteSpace(nonExistentId) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteSpace_ServiceError_ReturnsInternalServerError()
+        {
+            // Arrange
+            int sampleId = 1;
+
+            var spaceToDelete = new Space
+            {
+                Id = sampleId,
+                Name = "SpaceToDelete",
+                Pictures = new List<byte[]>(),
+                RoomId = 2
+            };
+
+            _spaceServiceMock.Setup(service => service.GetSpaceById(sampleId)).ReturnsAsync(spaceToDelete);
+            _spaceServiceMock.Setup(service => service.DeleteSpace(sampleId))
+                             .ThrowsAsync(new Exception("Simulated service error"));
+            var controller = new SpaceController(_spaceServiceMock.Object);
+
+            // Act
+            var result = await controller.DeleteSpace(sampleId) as ObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status500InternalServerError, result.StatusCode);
         }
     }
 }
